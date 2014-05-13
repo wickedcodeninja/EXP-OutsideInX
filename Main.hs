28457b668257e4d13bf2beb9ee053e1c5cfd6bb7
wickedcodeninja <wicked@codeninja>
2014-05-13 14:51:06 +0200

    Free variables for all

--- Main.hs
+++ Main.hs
@@ -2,17 +2,18 @@
 
 import Prelude hiding ( )
 
-import Data.Foldable
+import Data.Foldable hiding ( msum )
 import Control.Monad
+import Control.Monad.State ( StateT (..) )
 import Control.Applicative
 
+import Text.Read
 
+import Data.Char
 import Data.Set ( Set (..), union, isSubsetOf, intersection )
+
 import qualified Data.Set as Set
 
-
-
-type Var = ()
 
 -- Always associate to right? Maybe 
 data Constraint = And Constraint Constraint | Empty
@@ -29,24 +30,57 @@ implicationPart :: ExtendedConstraint -> [ImplicationConstraint]
 implicationPart (_, i) = i
 
 type Toplevel = ()
-type Touchables = Set Var
+type Touchables = Set FUV
 
 
 type Subst = ()
 
 
 
-dom :: Subst -> Set Var  
+dom :: Subst -> Set FUV 
 dom = undefined  
   
-fuv :: Constraint -> Set Var
+fuv :: Constraint -> Set FUV
 fuv = undefined
+ 
+
+data FUV = FUV String
+  deriving (Show, Eq)
+instance Enum FUV where
+  toEnum n = if n >= 0 && n < 0 + lowercaseRange
+                then FUV $ [chr (ord 'a' + n)]
+                else FUV $ show (n - lowercaseRange) where
+    lowercaseRange = ord 'z' - ord 'a' + 1 
+  fromEnum (FUV s) = 
+    let lowercaseRange = ord 'z' - ord 'a' + 1
+        parseChar (x:[]) = if ord x >= ord 'a' && ord x <= ord 'z'
+                              then Just $ ord x - ord 'a'
+                              else Nothing
+        parseChar _ = Nothing
+    in case msum [ parseChar s :: Maybe Int
+                 , fmap (\t -> t + lowercaseRange) . readMaybe $ s :: Maybe Int
+                 ] of Just n -> n
+                      _      -> error "fromEnum: invalid FUV" 
+instance Ord FUV where
+  compare a b = compare (fromEnum a) (fromEnum b)
+       
   
-type Solver a = Either String a
+  
+data Error a = Error String | Value a
+ deriving Show
+instance Functor Error where
+  fmap f (Error a) = Error a
+  fmap f (Value a) = Value (f a)
+instance Monad Error where
+  Error a >>= f = Error a
+  Value a >>= f = f a
+  
+  return = Value
+  fail = Error
+  
+type Solver a = StateT FUV Error a
 
-oops :: String -> Solver a
-oops = Left 
-
+                                      
 class SubstApply a where
   applySubst :: Subst -> a -> a
 
@@ -65,10 +99,13 @@ instance SubstApply Constraint where
 assert :: Bool -> String -> Solver ()
 assert b s = if b then return () else error s
  
-(#) :: Set Var -> Set Var -> Bool
+(#) :: Set FUV -> Set FUV -> Bool
 a # b = Set.null (a `intersection` b) 
  
 
+free :: Solver FUV
+free = StateT $ \fuv -> Value (fuv, succ fuv)
+ 
 solver :: Toplevel                    --Top-level constraints
        -> Constraint                  --Q_Given
        -> Touchables                  --Touchables
@@ -86,7 +123,7 @@ solver tl given tch wanted = do
         (r_i, subst_i) <- solver tl (given `And` (residual `And` q_i)) tch_i c_i
         
         when (r_i /= Empty) $ 
-          oops $ "solver: could not fully discharge implication constraint because <insert reason here>"
+          fail $ "solver: could not fully discharge implication constraint because <insert reason here>"
   
   -- Recursively solve all the implication constraints, discarding generated substitutions
   -- which are internal to the implication constraint.
